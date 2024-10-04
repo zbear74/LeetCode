396310e5647d8a2d4e2765af6989843d9e2541c7
zbear74 <mzolotuhin@gmail.com>
2024-10-04 11:01:01 -0700

    LongerstCommonPrefix changed - approved on LeetCode

--- easy/LongestCommonPrefix.py
+++ easy/LongestCommonPrefix.py
@@ -3,18 +3,29 @@
 
 strs1 = ["flower","flow","flight"]
 strs2 = ["dog","racecar","car"]    
-
-class Solution():
-    def longestCommonPrefix(self, l_str) -> str:
-        result =""
-        template_str = l_str[0]
+strs3 = ["cir","car"]
+strs4 = ["flower","fkow"]
+strs5 = ["abab","aba",""]
+strs6 = ["acbb","a"]
+           
+class Solution:
+    def longestCommonPrefix(self, l_str) -> str:        
+        result = l_str[0]
         for item in l_str:
-            result=""   
-            for i in range(0,len(item)):
-                if i< len(template_str) and item[i] ==  template_str[i]:
-                    result += item[i]                
-            template_str= result
-        return result            
+            if result == "":
+                return ""
+            for i in range(len(result),0,-1):                
+                if item.startswith(result):                                        
+                    break
+                else:
+                    result = result[:len(result)-1]
+        return result     
+
+a = Solution().longestCommonPrefix(strs6)
 
 print("res = [",Solution().longestCommonPrefix(strs1),"]")
 print("res = [",Solution().longestCommonPrefix(strs2),"]")
+print("res = [",Solution().longestCommonPrefix(strs3),"]")
+print("res = [",Solution().longestCommonPrefix(strs4),"]")
+print("res = [",Solution().longestCommonPrefix(strs5),"]")
+print("res = [",Solution().longestCommonPrefix(strs6),"]")
