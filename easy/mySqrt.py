eca7e44310e947c6c0284819789ba1694cab474c
zbear74 <mzolotuhin@gmail.com>
2024-10-18 13:48:54 -0700

    add mySqrt.py after approve LeetCode

---
+++ easy/mySqrt.py
@@ -0,0 +1,21 @@
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+
+class Solution:        
+    def mySqrt(self, l: int) -> int:
+        rslt = l
+        div = l
+        if l <= 0:
+            return 0
+        while True:
+            div =  (l / div + div) / 2
+            if rslt > div:
+                rslt = div
+            else:
+                return int(rslt)
+      
+
+
+print(Solution().mySqrt(4))
+print(Solution().mySqrt(8))
+  
